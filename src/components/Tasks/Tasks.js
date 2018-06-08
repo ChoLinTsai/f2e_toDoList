1edbe40c22763fb2906b2fe2f380eeb5e8f8f36f
Cho Lin Tsai <max19851113@gmail.com>
2018-06-08 20:24:45 +0800

    task deadline block finished

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -3,33 +3,99 @@ import './tasks.scss';
 import { 
   Container,
   Collapsible,
-  CollapsibleItem } from 'react-materialize';
+  Row,
+  Input } from 'react-materialize';
 
 export default class Tasks extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
+
   render() {
     return (
+
       <div className="sectionTasks">
         <Container>
-          <div className="tasksBlock">
 
+          <div className="tasksBlock">
             <Collapsible>
               <li>
                 <div className="collapsible-header">
-                  565656565
+
+                  <div className="checkboxWrapper">
+                    <div className="row">
+                      <div className="col">
+                        <input id="input_1" type="checkbox"/>
+                        <label htmlFor="input_1"></label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="taskTitleWrapper">
+                    <div className="row">
+                      <form action="#" className="col s12">
+                        <div className="row">
+                          <div className="input-field col s12">
+                            <input id="last_name"
+                              type="text" className="validate" />
+                            <label htmlFor="last_name">Task Name</label>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+                  <div className="taskIconWrapper">
+                    <div className="row">
+                      <i className="material-icons iconStar">star_border</i>
+                    </div>
+                    
+                    <i className="material-icons iconEdit">create</i>
+                  </div>
                   
                 </div>
+
                 <div className="collapsible-body">
-                    <span>Lorem ipsum dolor sit amet.</span>
+
+                  <div className="taskContent">
+                    <div className="deadlineBlock">
+                      <div className="deadlineTitle">
+                        <i className="material-icons iconDate">
+                          date_range
+                        </i>
+                        <h6>Deadline</h6>
+                      </div>
+
+                      <div className="deadlineDateTime">
+
+                        <div className="inputDateBlock">
+                          <Input s={6} placeholder="yyyy/mm/dd"
+                            className="inputDate"
+                            label="Enter a Date" />
+                        </div>
+
+                        <div className="inputTimeBlock">
+                          <Input s={6} placeholder="hh:mm"
+                              className="inputTime"
+                              label="Enter a Time/24hrs" />
+                        </div>
+
+                      </div>
+                    </div>
+                    <div className="fileBlock"></div>
+                    <div className="commentBlock"></div>
                   </div>
+
+                  <div className="taskContentFooter">
+                  </div>
+                </div>
+
               </li>
 
             </Collapsible>
-
           </div>
+
         </Container>
       </div>
     )
